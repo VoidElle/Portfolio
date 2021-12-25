5aabfe1379ce3503b485775cba05167910785e5a
Luca <lucadelc@gmail.com>
2021-12-25 12:14:40 +0100

    💡 Added the heading and the base of the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import { ChakraProvider} from "@chakra-ui/provider";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+      <ChakraProvider>
+        <Head>
+          <title>Luca Del Corona | Portfolio</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+          <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
