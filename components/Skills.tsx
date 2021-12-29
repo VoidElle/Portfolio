c69088a73d7f67df8f1cfeb4edfba728e0835a7d
Luca <lucadelc@gmail.com>
2021-12-29 23:03:52 +0100

    🔖 Inserted the skills data inside a json file

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,5 @@
 import { Container, Heading, Flex, Badge } from "@chakra-ui/react";
+import skills from "../data/skills.json";
 
 /**
  * The properties of a skill component.
@@ -20,6 +21,25 @@ interface SkillsProps {
 }
 
 /**
+ * The interface of a Skill.
+ * @interface Skill
+ */
+interface Skill {
+
+    /**
+     * The name of the skill.
+     * @type {string}
+     */
+    name: string;
+
+    /**
+     * The color of the skill.
+     * @type {number}
+     */
+    color: string;
+}
+
+/**
  * The component that represents a Skill.
  * @param props The properties of the component
  */
@@ -30,21 +50,13 @@ const Skills = (props: SkillsProps) => {
                 My Skills 💪
             </Heading>
             <Flex justifyContent="start" alignItems="center" direction="row" wrap="wrap" gap={4}>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="teal">Dart</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="messenger">Flutter</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="yellow">Java</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="red">Javascript</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="cyan">Node.js</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="green">HTML</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="blue">CSS</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="blue">Docker</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme={"blackAlpha"}>GitHub Actions</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="purple">Bootstrap</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="green">MongoDB</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="yellow">MySQL</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="cyan">PostgreSQL</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="orange">MariaDB</Badge>
-                <Badge mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme="orange">Git</Badge>
+                {
+                    skills.list.map((i: Skill) => {
+                        return(
+                            <Badge key={i.name} mx={1} my={1} fontSize={{ base: '0.9em', md: '1em' }} colorScheme={i.color}>{i.name}</Badge>
+                        );
+                    })
+                }
             </Flex>
         </Container>
     );
